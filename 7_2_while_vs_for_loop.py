7c15eaebc4bbf6a3fe90a89b496b035272ad3d02
mortazakiyani <36876384+mortazakiyani@users.noreply.github.com>
2018-03-03 20:58:56 +0330

    Update 7_2_while_vs_for_loop.py

--- 7_2_while_vs_for_loop.py
+++ 7_2_while_vs_for_loop.py
@@ -1,11 +1,11 @@
 year=int(input("please enter year\n"))
 temp_year=year
 while year<2029:
-    year+=1
+    year+=3
     print(year)
 
 print("inside for loop")
 
 year=int(input("please enter year\n"))
-for x in range(temp_year,2029,2):
+for x in range(temp_year,2029,5):
     print(x)
